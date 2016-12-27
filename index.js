1f71d6351426d7398750fb9a78b74256602d2cb3
Travis J. Webb <me@traviswebb.com>
2016-12-27 13:28:48 -0500

    [api] initial setup using runkit/json-endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Trailpack = require('trailpack')
+const runkitEndpoint = require('@runkit/runkit/json-endpoint/1.0.0')
 
 module.exports = class RunkitTrailpack extends Trailpack {
 
@@ -8,21 +9,23 @@ module.exports = class RunkitTrailpack extends Trailpack {
    * TODO document method
    */
   validate () {
-
+    if (!this.app.config.runkit.context) {
+      throw new ConfigValueError('config.runkit.context must be properly set')
+    }
+    if (!this.app.controllers.RunkitController) {
+      throw new ConfigValueError('RunkitController must be defined')
+    }
   }
 
   /**
-   * TODO document method
-   */
-  configure () {
-
-  }
-
-  /**
-   * TODO document method
+   * Create the runkit endpoint from the RunkitController.runkitEndpoint handler
    */
   initialize () {
+    const RunkitController = this.app.controllers.RunkitController
 
+    return runkitEndpoint(this.app.config.runkit.context, (req, done) => {
+      done(RunkitController.runkitEndpoint(req))
+    })
   }
 
   constructor (app) {
