4fd2c0bffa2d147304267dc373d4ee268d867579
Travis J. Webb <me@traviswebb.com>
2016-12-28 14:19:51 -0500

    [fix] endpoint cannot be set in initialize

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const Trailpack = require('trailpack')
-//const runkitEndpoint = require('@runkit/runkit/json-endpoint/1.0.0')
 
 module.exports = class RunkitTrailpack extends Trailpack {
 
@@ -14,7 +13,7 @@ module.exports = class RunkitTrailpack extends Trailpack {
   /**
    * Create the runkit endpoint from the RunkitController.runkitEndpoint handler
    */
-  initialize () {
+  configure () {
     const RunkitController = this.app.controllers.RunkitController
     const context = this.app.config.runkit.context
 
